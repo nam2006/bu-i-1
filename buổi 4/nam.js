6015c9e962ac9f0d330acf3a30855f442673e4be
nam2006 <105550694+nam2006@users.noreply.github.com>
2022-09-09 21:05:47 +0700

    checkpoint buoi4

---
+++ buổi 4/nam.js
@@ -0,0 +1,13 @@
+  
+        N = parseInt(10)
+       
+        for(i=0;i<N;i++) {
+            
+            for(j=0;j<=i;j++) {
+                document.writeln(" 1 ")
+            }
+    
+           
+            document.writeln("<br/>")
+        }
+        document.getElementById{function}
---
+++ sản phẩm cuối khóa/btvn  buổi 9/p.js
@@ -0,0 +1,1 @@
+image.pnghttps://anhdepfree.com/wp-content/uploads/2020/10/hinh-anh-la-co-quoc-ki-cua-viet-nam.jpg
