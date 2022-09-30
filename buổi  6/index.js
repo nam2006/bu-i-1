b1fbc2cef820f075886bd73aa6a3ed1731925ae4
nam2006 <105550694+nam2006@users.noreply.github.com>
2022-09-30 19:40:28 +0700

    bt buoi6

---
+++ buổi  6/index.js
@@ -0,0 +1,18 @@
+let giohang = {
+    apple:0,
+    melon:0,
+    mango:0,
+}
+function click_apple() {
+    giohang.apple ++
+    console.log(giohang)
+}
+function click_melon() {
+    giohang.melon = giohang.melon + 1
+    console.log(giohang)
+}
+function click_mango() {
+    giohang.mangos +=1
+    console.log(giohang)
+}
+console.log("mua 1 mango")
